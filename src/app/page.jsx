1e655dcc70a6dd2ce84021683020581bbdeb17da
Sidharth Soman <sidharthsoman065@gmail.com>
2025-03-28 16:44:44 +0530

    added form

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
 const page = () => {
+  const [text, setText] = React.useState("Enter your time data here...");
+
   return (
-    <div>
-       
+    <div className="h-screen w-full flex justify-center items-center bg-cover bg-center ">
+       <div className="h-[60%] w-[35%] outline-offset-2 border-2 p-5">
+          <h1 className='text-4xl text-black'>Time Calculator</h1>
+             <div className='mt-5'>
+              <form action="">
+                <div className="flex flex-col">
+                <label for="story">Enter your time data:</label>
+                <textarea id="story" name="story" rows="8" cols="10" value ={text} className='mt-4'>
+                   
+                </textarea>
+                <button>
+                  Calculate1
+                </button>
+                </div>
+              </form>
+            </div>    
+       </div>
     </div>
   )
 }
